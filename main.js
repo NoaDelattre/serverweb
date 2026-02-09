581470d8e2896c6871cfe79949c5bca697634f20
NoaDelattre <lamarcknoadelattre@gmail.com>
2026-02-09 14:19:24 +0100

    exercice 2

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 alert("Hello World!");
+
+//chaine de caractere
+var MaVariableString = "toto";
+//nombre
+var MaVariableNumerique = 100;
+//des tableaux
+var MaVariabletableau = ['Apple', 'Banana'];
+//des objets (ici la fenetre du navigateur )
+var MaVariableObjet = window;
+
+alert(MaVariableString + MaVariableNumerique + MaVariabletableau[0] + MaVariableObjet.location.pathname);
