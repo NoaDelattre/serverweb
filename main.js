16675a5096b4f1910b5e50546172dedfd5363eb5
NoaDelattre <lamarcknoadelattre@gmail.com>
2026-03-04 11:39:23 +0100

    exercice 7

--- main.js
+++ main.js
@@ -3,18 +3,16 @@ alert("Hello World!");
 // Saisie nombre
 var N = prompt("Entrer un nombre");
 var resultat = 0;
+var j = 0;
 function addition(param1, param2){
-    resultat = resultat + i;
+    resultat = resultat + j;
 };
 
 // Boucle for Calcul factoriel de 1 à N
 for (let i = 1; i <= N; i++){
-    addition();
-}
+    j = i;
+    addition();   
+};
 
 // Afficher le résultat
 alert(resultat);
-
-
-
-return 0;
