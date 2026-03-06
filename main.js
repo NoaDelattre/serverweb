5ca2aa1e1a80d5e3d603b601bdd30d062f37a104
NoaDelattre <lamarcknoadelattre@gmail.com>
2026-03-06 12:47:44 +0100

    première update exercice 9

--- main.js
+++ main.js
@@ -15,9 +15,9 @@ var N = prompt("Entrer un nombre");
 
 function ModifText(letext){
     alert("je vais modifier les divs");
-    const mesDivs = document.getElementsByName("lesdivs");
+    const mesDivs = document.getElementsByName(div,"lesdivs");
     for (let i = 0; i < N; i++){        
-        console.log(mesDivs[0].tagName);
+        console.log(mesDivs[i].tagName);
         mesDivs.textContent = letext;
     }   
 }
